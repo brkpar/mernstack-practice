f9661acf1d3f7f3b31683bee7db4a9f0f6b7afaf
Dongwan Kim <brkpar@gmail.com>
2020-05-11 00:57:48 -0700

    mongoDB credential protection

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://donkim:ehddhks1@mernstack-practice-eqqjt.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!반갑다'))
+app.get('/', (req, res) => res.send('Hello World!반갑다 진짜다 가짜다'))
+
+app.post('/register', (req, res) => {
+    //회원가입시 필요한 정보들을 client에서 가져오면
+    //그것들을 DB에 넣어준다.
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.jason({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
